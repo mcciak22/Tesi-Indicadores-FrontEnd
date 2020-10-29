fdc33503299ce47b8c861d9748d72f8dac8b9f68
Mario Rodriguez <rodriguez.mario@cuallisys.com>
2020-10-28 20:50:54 -0600

    add and deleted router module

---
+++ src/app/main/pages/pages.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ForgotpasswordComponent } from './authentication/forgotpassword/forgotpassword.component';
+
+
+
+@NgModule({
+  declarations: [ForgotpasswordComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
