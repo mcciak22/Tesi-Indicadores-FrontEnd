642e74dadda8b40acd03f06e4acc7884befaa14f
Mario Rodriguez <rodriguez.mario@cuallisys.com>
2020-11-21 10:46:41 -0600

    commit de prueba

--- src/app/main/pages/menuprincipal/menuprincipal.component.ts
+++ src/app/main/pages/menuprincipal/menuprincipal.component.ts
@@ -4,23 +4,14 @@ import { NameService } from 'src/app/core/_services/servicio-cambionombre/name.s
 @Component({
   selector: 'menuprincipal',
   templateUrl: './menuprincipal.component.html',
-  styleUrls: ['./menuprincipal.component.scss']
+  styleUrls: ['./menuprincipal.component.scss'],
 })
 export class MenuPrincipalComponent implements OnInit {
-  @Output() EmitirEvento:EventEmitter<any> = new EventEmitter()
-  titulopagina: string = 'Menu Principal';
+  @Output() EmitirEvento: EventEmitter<any> = new EventEmitter();
+  titulopagina = 'Menu Principal';
 
-  constructor(
-    public nameService:NameService
-  ) 
-  { 
-    this.nameService.titulopagina = this.titulopagina
-  }  
-  ngOnInit(): void {
-    
-   
+  constructor(public nameService: NameService) {
+    this.nameService.titulopagina = this.titulopagina;
   }
- 
-  
-  
+  ngOnInit(): void {}
 }
